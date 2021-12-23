ad296a5c75a8b312bd7406041235da4ebdcbc0a8
Benjamin Davies <bentendavies@gmail.com>
2021-12-23 22:32:51 +1300

    Add rust port

---
+++ plover-evdevd-rs/src/evdev.rs
@@ -0,0 +1,228 @@
+// This file might be a derivative work of https://github.com/ndesh26/evdev-rs
+
+use evdev_sys as sys;
+use glob::glob;
+use std::{
+    ffi::CStr,
+    fs::File,
+    io,
+    os::{raw::c_uint, unix::io::AsRawFd},
+    path::Path,
+    ptr,
+};
+
+macro_rules! unsafe_io {
+    ($x:expr) => {{
+        let res = unsafe { $x };
+        if res < 0 {
+            Err(io::Error::from_raw_os_error(-res))?
+        } else {
+            res
+        }
+    }};
+}
+
+pub struct Device {
+    _file: File,
+    raw: *mut sys::libevdev,
+    name: String,
+    grabbed: bool,
+}
+
+impl Device {
+    pub fn list() -> impl Iterator<Item = Self> {
+        glob("/dev/input/event*")
+            .unwrap()
+            .filter_map(|path| Some(Device::open(path.ok()?).ok()?))
+    }
+
+    pub fn open<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let file = File::open(path)?;
+
+        let mut raw = ptr::null_mut();
+        unsafe_io!(sys::libevdev_new_from_fd(file.as_raw_fd(), &mut raw));
+
+        let raw_name = unsafe { sys::libevdev_get_name(raw) };
+        let name = unsafe { CStr::from_ptr(raw_name) }
+            .to_str()
+            .unwrap()
+            .to_owned();
+
+        Ok(Self {
+            _file: file,
+            raw,
+            name,
+            grabbed: false,
+        })
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn has_key(&self, key: Key) -> bool {
+        let Key(code) = key;
+        unsafe { sys::libevdev_has_event_code(self.raw, EV_KEY as c_uint, code as c_uint) != 0 }
+    }
+
+    pub fn grab(&mut self) -> io::Result<()> {
+        unsafe_io!(sys::libevdev_grab(self.raw, sys::LIBEVDEV_GRAB));
+        self.grabbed = true;
+        Ok(())
+    }
+
+    pub fn ungrab(&mut self) -> io::Result<()> {
+        unsafe_io!(sys::libevdev_grab(self.raw, sys::LIBEVDEV_UNGRAB));
+        self.grabbed = false;
+        Ok(())
+    }
+
+    pub fn read_loop(&mut self) -> DeviceReadLoop {
+        DeviceReadLoop(self)
+    }
+}
+
+unsafe impl Send for Device {}
+
+impl Drop for Device {
+    fn drop(&mut self) {
+        if self.grabbed {
+            self.ungrab().unwrap();
+        }
+        unsafe {
+            sys::libevdev_free(self.raw);
+        }
+    }
+}
+
+pub struct DeviceReadLoop<'a>(&'a mut Device);
+
+impl<'a> Iterator for DeviceReadLoop<'a> {
+    type Item = Event;
+
+    fn next(&mut self) -> Option<Event> {
+        let mut ev = sys::input_event {
+            time: sys::timeval {
+                tv_sec: 0,
+                tv_usec: 0,
+            },
+            type_: 0,
+            code: 0,
+            value: 0,
+        };
+        unsafe {
+            sys::libevdev_next_event(
+                self.0.raw,
+                (sys::LIBEVDEV_READ_FLAG_NORMAL | sys::LIBEVDEV_READ_FLAG_BLOCKING) as c_uint,
+                &mut ev,
+            );
+        }
+        Some(ev.into())
+    }
+}
+
+pub struct UInput {
+    raw: *mut sys::libevdev_uinput,
+}
+
+impl UInput {
+    pub fn from_device(dev: &Device) -> io::Result<Self> {
+        let mut raw = ptr::null_mut();
+        unsafe_io!(sys::libevdev_uinput_create_from_device(
+            dev.raw,
+            sys::LIBEVDEV_UINPUT_OPEN_MANAGED,
+            &mut raw
+        ));
+
+        Ok(Self { raw })
+    }
+
+    pub fn write_event(&mut self, event: Event) -> io::Result<()> {
+        let ev: sys::input_event = event.into();
+        unsafe_io!(sys::libevdev_uinput_write_event(
+            self.raw,
+            ev.type_ as c_uint,
+            ev.code as c_uint,
+            ev.value
+        ));
+        Ok(())
+    }
+
+    pub fn syn(&mut self) -> io::Result<()> {
+        self.write_event(Event::Syn)
+    }
+}
+
+unsafe impl Send for UInput {}
+
+impl Drop for UInput {
+    fn drop(&mut self) {
+        unsafe {
+            sys::libevdev_uinput_destroy(self.raw);
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Event {
+    Syn,
+    Key(Key, bool),
+    Other(u16, u16, i32),
+}
+
+impl From<sys::input_event> for Event {
+    fn from(ev: sys::input_event) -> Self {
+        match ev.type_ {
+            EV_SYN => Self::Syn,
+            EV_KEY => Self::Key(Key(ev.code), ev.value != 0),
+            t => Self::Other(t, ev.code, ev.value),
+        }
+    }
+}
+
+impl Into<sys::input_event> for Event {
+    fn into(self: Event) -> sys::input_event {
+        let time = sys::timeval {
+            tv_sec: 0,
+            tv_usec: 0,
+        };
+        match self {
+            Self::Syn => sys::input_event {
+                time,
+                type_: EV_SYN,
+                code: 0,
+                value: 0,
+            },
+            Self::Key(Key(code), value) => sys::input_event {
+                time,
+                type_: EV_KEY,
+                code,
+                value: if value { 1 } else { 0 },
+            },
+            Self::Other(type_, code, value) => sys::input_event {
+                time,
+                type_,
+                code,
+                value,
+            },
+        }
+    }
+}
+
+const EV_SYN: u16 = 0;
+const EV_KEY: u16 = 1;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Key(pub u16);
+
+impl Key {
+    pub const KEY_A: Key = Key(30);
+    pub const KEY_LEFTSHIFT: Key = Key(42);
+    pub const KEY_RIGHTSHIFT: Key = Key(54);
+    pub const KEY_LEFTCTRL: Key = Key(29);
+    pub const KEY_RIGHTCTRL: Key = Key(97);
+    pub const KEY_LEFTALT: Key = Key(56);
+    pub const KEY_RIGHTALT: Key = Key(100);
+    pub const KEY_LEFTMETA: Key = Key(125);
+    pub const KEY_RIGHTMETA: Key = Key(126);
+}
---
+++ plover-evdevd-rs/src/main.rs
@@ -0,0 +1,129 @@
+mod evdev;
+
+use evdev::*;
+use std::{
+    collections::HashSet,
+    io::{self, BufRead, Write},
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
+const UINPUT_NAME: &str = "Plover";
+
+const MODIFIER_KEYS: [Key; 8] = [
+    Key::KEY_LEFTSHIFT,
+    Key::KEY_RIGHTSHIFT,
+    Key::KEY_LEFTCTRL,
+    Key::KEY_RIGHTCTRL,
+    Key::KEY_LEFTALT,
+    Key::KEY_RIGHTALT,
+    Key::KEY_LEFTMETA,
+    Key::KEY_RIGHTMETA,
+];
+
+fn is_keyboard(dev: &Device) -> bool {
+    dev.name() != UINPUT_NAME && dev.has_key(Key::KEY_A)
+}
+
+fn find_first_keyboard() -> Device {
+    Device::list()
+        .find(is_keyboard)
+        .expect("No keyboards found")
+}
+
+fn listen_kb(mut dev: Device, uinput: Arc<Mutex<UInput>>, suppress_keys: Arc<Mutex<HashSet<Key>>>) {
+    let stdout = io::stdout();
+    // stdout is only used in this method, so we may as well only lock it once
+    let mut stdout = stdout.lock();
+
+    let mut modifiers = Vec::new();
+
+    for event in dev.read_loop() {
+        if let Event::Key(key, pressed) = event {
+            if MODIFIER_KEYS.contains(&key) {
+                if pressed {
+                    if modifiers.contains(&key) {
+                        modifiers.push(key);
+                    }
+                } else {
+                    modifiers.retain(|k| k != &key);
+                }
+            }
+
+            if (*suppress_keys.lock().unwrap()).contains(&key) && modifiers.len() == 0 {
+                let Key(code) = key;
+                let prefix = if pressed { 'd' } else { 'u' };
+                writeln!(stdout, "{}{}", prefix, code);
+                continue;
+            }
+        }
+
+        {
+            let mut uinput = uinput.lock().unwrap();
+            (*uinput).write_event(event).unwrap();
+        }
+    }
+}
+
+fn listen_stdio(uinput: Arc<Mutex<UInput>>, suppress_keys: Arc<Mutex<HashSet<Key>>>) {
+    let stdin = io::stdin();
+    // stdin is only used in this method, so we may as well only lock it once
+    let mut stdin = stdin.lock();
+
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        stdin.read_line(&mut line).unwrap();
+
+        if let Some(first_char) = line.chars().next() {
+            let content = &line[1..line.len() - 1];
+            match first_char {
+                'd' => {
+                    if let Ok(code) = content.parse() {
+                        let mut uinput = uinput.lock().unwrap();
+                        (*uinput).write_event(Event::Key(Key(code), true)).unwrap();
+                    }
+                }
+                'u' => {
+                    if let Ok(code) = content.parse() {
+                        let mut uinput = uinput.lock().unwrap();
+                        (*uinput).write_event(Event::Key(Key(code), false)).unwrap();
+                    }
+                }
+                's' => {
+                    let mut new_suppressed = HashSet::new();
+                    for substr in content.split(' ') {
+                        if let Ok(code) = substr.parse() {
+                            new_suppressed.insert(Key(code));
+                        }
+                    }
+                    let mut suppress_keys = suppress_keys.lock().unwrap();
+                    *suppress_keys = new_suppressed;
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+fn main() {
+    thread::sleep(Duration::from_millis(500));
+
+    let mut dev = find_first_keyboard();
+    dev.grab().unwrap();
+    eprintln!("Using device: {}", dev.name());
+
+    let uinput = Arc::new(Mutex::new(UInput::from_device(&dev).unwrap()));
+    let uinput_ = uinput.clone();
+
+    let suppress_keys = Arc::new(Mutex::new(HashSet::new()));
+    let suppress_keys_ = suppress_keys.clone();
+
+    thread::spawn(|| listen_kb(dev, uinput, suppress_keys));
+
+    thread::spawn(|| listen_stdio(uinput_, suppress_keys_))
+        .join()
+        .unwrap();
+}
